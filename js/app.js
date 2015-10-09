3c78bf18caee8eb25578df793b259d1f15ab2c19
Ozzy <ozzytriff@gmail.com>
2015-10-09 12:03:04 -0400

    rough draft, fixed score issue

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	var quizQuestions = [{
 	question:"In what year was America founded ?",
 	choices:["1775", "1776", "1801","1492"],
-	answer: 1 
+	answer: 1
 },
 {
 	question:"Who did not sign the Declaration of Independence ?",
@@ -33,25 +33,46 @@ $(document).ready(function() {
 	$("#start").click(function() {
 			$("#start").hide()
 			$("#next").show()
-	})
+	});	
 	
 	$("#next").on("click", function(){
 		
 				$(".choices, .questions").empty();
 
-			function incrementCounter(){
+	  function incrementCounter(){
 				$( "#count").text(counter);
 			};	
-				
+				if(quizQuestions[counter]){
 				$(".questions").append("<h2>" + quizQuestions[counter].question + "</h2>")
 
 	for(var i = 0 ; i < quizQuestions[counter].choices.length;i+=1){
-		$(".choices").append( "<ul>" + "<input type='radio'/>" + quizQuestions[counter].choices[i] + "</ul>")
-	}				
+		$(".choices").append( "<ul>" + "<input type='radio' name='radio' value=' " + i + " '/>" + quizQuestions[counter].choices[i] + "</ul>");
+	}	
 
 incrementCounter();
-counter++
-})
+counter++ 
+			}
+		else {
+			$("#score").text("score:" + score);
+		}
+
+});
 
 
+		$("body").on("click","input", function() {
+
+					$("input[type='radio']:checked").val();
+
+					var $selectedText=$("input[type='radio']:checked").val();
+
+					if($selectedText==quizQuestions[counter-1].answer) {
+						score +=1;
+					}
+			
+					if(counter > quizQuestions.length) {  
+					  
+					}
+					
+		})
+	
 });
